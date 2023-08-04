424370fccc5b71b331a8e29e745c8acc1b1f35e6
Dominique Bonifacio <000448188@student.vcc.ca>
2023-08-04 11:05:27 -0700

    Database connected with the page

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,8 +1,10 @@
 ﻿using iConnect.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
@@ -11,15 +13,50 @@ namespace iConnect.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        public IConfiguration Configuration { get; }
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(IConfiguration configuration)
         {
-            _logger = logger;
+            Configuration = configuration;
         }
 
         public IActionResult Index()
         {
+            string connectionString = Configuration["ConnectionStrings:DefaultConnection"];
+            SqlConnection connection = new SqlConnection(connectionString);
+            string sql = "SELECT * FROM Contact";
+            SqlCommand command = new SqlCommand(sql, connection);
+
+            connection.Open();
+            using (SqlDataReader dataReader = command.ExecuteReader())
+            {
+                do
+                {
+
+                    while (dataReader.Read())
+                    {
+                        // HARD CODED WAY OF READING DATA
+                        //int contactID = Convert.ToInt32(dataReader["ContactID"]);
+                        //string firstName = Convert.ToString(dataReader["FirstName"]);
+                        //string lastName = Convert.ToString(dataReader["LastName"]);
+                        //string email = Convert.ToString(dataReader["Email"]);
+                        //string phone = Convert.ToString(dataReader["Phone"]);
+                        //string organization = Convert.ToString(dataReader["Organization"]);
+                        //string relationship = Convert.ToString(dataReader["Relationship"]);
+                        //string group = Convert.ToString(dataReader["Group"]);
+
+                        for (int i = 0; i < dataReader.FieldCount; i++)
+                        {
+                            string currentColName = dataReader.GetName(i);
+                            string currentColVal = Convert.ToString(dataReader.GetValue(i));
+                        }
+
+
+                    }
+                } while (dataReader.NextResult());
+            }
+            connection.Close();
+
             return View();
         }
 
---
+++ Models/Contact.cs
@@ -0,0 +1,14 @@
+﻿namespace iConnect.Models
+{
+    public class Contact
+    {
+        public string ContactID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Organization { get; set; }
+        public string Relationship { get; set; }
+        public string Group { get; set; }
+    }
+}
